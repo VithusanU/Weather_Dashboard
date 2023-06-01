d33da9911c07f9fd2e780df979e01c85e2d74cc6
VithusanU <130177580+VithusanU@users.noreply.github.com>
2023-05-31 23:34:48 -0400

    New changes

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,11 @@
 $(document).ready(function () {
-    //search button feature
     $("#search-button").on("click", function () {
-      //get value in input search-value.
       var searchTerm = $("#search-value").val();
-      //empty input field.
       $("#search-value").val("");
       weatherFunction(searchTerm);
       weatherForecast(searchTerm);
     });
   
-    //search button enter key feature. 
     $("#search-button").keypress(function (event) {
       var keycode = (event.keyCode ? event.keyCode : event.which);
       if (keycode) {
@@ -18,7 +14,6 @@ $(document).ready(function () {
       }
     });
   
-    //pull previous searches from local storage
     var history = JSON.parse(localStorage.getItem("history")) || [];
   
 })
